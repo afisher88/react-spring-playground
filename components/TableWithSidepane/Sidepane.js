7648a50df451fe3773a057ca95bbb66143ce00f0
Anthony Fisher <Anthony.Fisher@motors.internal>
2018-11-12 11:37:25 +0000

    Change sidepane to component from fn component

--- components/TableWithSidepane/Sidepane.js
+++ components/TableWithSidepane/Sidepane.js
@@ -1,39 +1,54 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { animated, config, Transition } from 'react-spring';
 
-const openConfigEnter = { transform: 'translate3d(0,0,0)' };
-const openConfigLeave = { transform: 'translate3d(100%,0,0)' };
-const updateConfigEnter = [{ opacity: 0, transform: 'translate3d(0,0,0)' },{ opacity: 1, transform: 'translate3d(0,0,0)' }];
-const updateConfigLeave = [{ opacity: 0, transform: 'translate3d(0,0,0)' }];
+export default class Sidepane extends Component {
+  constructor() {
+    super();
 
-export default function Sidepane({ activeRowIndex, tableData, hasUpdated }) {
-  let enter = updateConfigEnter;
-  let leave = updateConfigLeave;
-
-  if (!hasUpdated) {
-    enter = openConfigEnter;
-    leave = openConfigLeave;
+    this.openConfigEnter = { transform: 'translate3d(0,0,0)' };
+    this.openConfigLeave = { transform: 'translate3d(100%,0,0)' };
+    this.updateConfigEnter = [{ opacity: 0, transform: 'translate3d(0,0,0)' }, { opacity: 1, transform: 'translate3d(0,0,0)' }];
+    this.updateConfigLeave = [{ opacity: 0, transform: 'translate3d(0,0,0)' }];
   }
 
-  return (
-    <Transition
-      native
-      items={activeRowIndex}
-      from={leave}
-      enter={enter}
-      leave={leave}
-      config={config.slow}
-    >
-      {activeRowIndex =>
-        typeof activeRowIndex === 'number' && (props => (
-          <animated.div className="sidepane-animation-container" style={props}>
-            <article className="sidepane">
-              <h2>{tableData[activeRowIndex].name}</h2>
-              <p>{`${tableData[activeRowIndex].name} is a ${tableData[activeRowIndex].age} year old ${tableData[activeRowIndex].job} living in ${tableData[activeRowIndex].location}.`}</p>
-            </article>
-          </animated.div>)
-        )
-      }
-    </Transition>
-  )
+
+  shouldComponentUpdate(nextProps) {
+    const { activeRowIndex, tableData } = this.props;
+
+    return activeRowIndex !== nextProps.activeRowIndex;
+  }
+
+
+  render() {
+    const { activeRowIndex, tableData, hasUpdated } = this.props;
+
+    let enter = this.updateConfigEnter;
+    let leave = this.updateConfigLeave;
+
+    if (!hasUpdated) {
+      enter = this.openConfigEnter;
+      leave = this.openConfigLeave;
+    }
+
+    return (
+      <Transition
+        native
+        items={activeRowIndex}
+        from={leave}
+        enter={enter}
+        leave={leave}
+      >
+        {activeRowIndex =>
+          typeof activeRowIndex === 'number' && (props => (
+            <animated.div className="sidepane-animation-container" style={props}>
+              <article className="sidepane">
+                <h2>{tableData[activeRowIndex].name}</h2>
+                <p>{`${tableData[activeRowIndex].name} is a ${tableData[activeRowIndex].age} year old ${tableData[activeRowIndex].job} living in ${tableData[activeRowIndex].location}.`}</p>
+              </article>
+            </animated.div>)
+          )
+        }
+      </Transition>
+    )
+  }
 }
