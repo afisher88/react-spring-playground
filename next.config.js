bb9405bb08ba44b6a634dc8bef689f976982c14c
Anthony Fisher <Anthony.Fisher@motors.internal>
2018-11-12 11:46:56 +0000

    Update next config

--- next.config.js
+++ next.config.js
@@ -1,2 +1,6 @@
-const withSass = require('@zeit/next-sass');
-module.exports = withSass();
+// const withSass = require('@zeit/next-sass');
+
+module.exports = {
+  withSass: require('@zeit/next-sass'),
+  assetPrefix: process.env.NODE_ENV === 'production' ? '/react-spring-playground' : '',
+};
